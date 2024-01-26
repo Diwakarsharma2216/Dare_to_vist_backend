385fabb4047240d3120d7d56ef54374f10271e47
diwakarsharma02 <diwakar.mtx@gmail.com>
2024-01-26 20:12:06 +0530

    update at 1/26/2024

--- controllers/podcast.controller.ts
+++ controllers/podcast.controller.ts
@@ -1,10 +1,17 @@
-import { NextFunction } from "express";
+import { NextFunction, Request, Response } from "express";
 import ErrorHandling from "../utlis/ErrorHandling";
+import { podcastModel } from "../model/podcast.model";
+
+
+//  get Routes
 
 export const podcastdata=async(req:Request,res:Response,next:NextFunction)=>{
     try {
-        
+        const data=await podcastModel.find()
+        res.status(201).json({succes:true,data})
+      
     } catch (error:any) {
         return next(new ErrorHandling(error.message,400))
     }
 }
+
--- routes/podcast.routes.ts
+++ routes/podcast.routes.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import userRouter from "./user.routes";
+import { podcastdata } from "../controllers/podcast.controller";
+import { isAuthticated } from "../middleware/auth";
 
-const podRouter=express.Router()
 
-userRouter.get("/podcast",)
+const podcostRouter=express.Router()
+
+
+podcostRouter.get("/podcost",isAuthticated,podcastdata)
